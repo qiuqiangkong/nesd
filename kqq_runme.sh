2d677e8f5524268fdbcbeb72a6bf66f3e0c081e8
kongqiuqiang <kongqiuqiang@bytedance.com>
2023-02-07 17:24:15 +0800

    update

--- kqq_runme.sh
+++ kqq_runme.sh
@@ -3,6 +3,7 @@ WORKSPACE="/home/tiger/workspaces/nesd2"
 
 SAMPLE_RATE=24000
 
+######## VCTK free field
 # Paths
 for SPLIT in "train" "test"
 do  
@@ -26,3 +27,29 @@ CUDA_VISIBLE_DEVICES=1 python3 ./nesd/inference.py inference \
     --checkpoint_path="/home/tiger/workspaces/nesd2/checkpoints/train/config=01a,gpus=1/step=6000.pth" \
     --gpus=1
    
+######## DCASE 2021
+for SPLIT in "train" "test"
+do  
+    python3 ./nesd/dataset_creation/pack_audios_to_hdf5s/dcase2021_task3.py \
+        --dataset_dir="/home/tiger/datasets/dcase2021/task3" \
+        --split=$SPLIT \
+        --hdf5s_dir="${WORKSPACE}/hdf5s/dcase2021_task3/sr=${SAMPLE_RATE}/${SPLIT}" \
+        --sample_rate=$SAMPLE_RATE
+done
+
+CUDA_VISIBLE_DEVICES=6 python3 ./nesd/train.py train \
+    --workspace=$WORKSPACE \
+    --config_yaml="./kqq_scripts/train/configs/dcase2021_task3_01a.yaml" \
+    --gpus=1
+
+CUDA_VISIBLE_DEVICES=0 python3 ./nesd/inference.py inference_dcase2021_single_map \
+    --workspace=$WORKSPACE \
+    --config_yaml="./kqq_scripts/train/configs/dcase2021_task3_01b.yaml" \
+    --checkpoint_path="/home/tiger/workspaces/nesd2/checkpoints/train/config=dcase2021_task3_01b,gpus=1/step=10000.pth" \
+    --gpus=1
+
+ffmpeg -framerate 10 -i '_tmp/_zz_%03d.jpg' -r 30 -pix_fmt yuv420p 123.mp4
+
+python3 nesd/evaluate_dcase2021_task3.py process_mat_write_csv --csv=""
+
+python3 nesd/evaluate_yin.py
